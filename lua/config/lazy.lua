3334bfa4d7cd6f720ed40f3bf0bcd8c45b2b9deb
Daniel Toebe <dtoebe@protonmail.com>
2024-06-29 09:08:46 -0600

    feat: added base langs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,6 +11,13 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     -- import/override with your plugins
     { import = "plugins" },
   },
