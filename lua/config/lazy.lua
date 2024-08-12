dea3897dd0d1a7d28167f6d2491dbb4f42b04690
Daniel Toebe <dtoebe@protonmaail.com>
2024-08-12 10:15:42 -0600

    added code-runner

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.python" },
 
     { import = "plugins" },
   },
